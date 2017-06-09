447bdfd3a0a8c58dcee74f2b698fc5e080943812
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-09 16:54:38 +0530

    Create program10.py

--- program10.py
+++ program10.py
@@ -3,3 +3,7 @@ def file_read(python):
         txt = open(python)  
         print(txt.read())  
 file_read('python.txt')  
+'''output
+[root@test assignment2]# python program10.py
+Python Exercises
+Java Exercises'''
