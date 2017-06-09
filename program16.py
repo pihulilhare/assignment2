0862ef0b3a091a608ba925db26d384e9a667a1ce
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-09 17:46:32 +0530

    Create program16.py

--- program16.py
+++ program16.py
@@ -1,1 +1,6 @@
+#16 : Write a Python program to remove newline characters from a file
 
+def remove_newlines(python):  
+    filelist = open(python).readlines()  
+    return [s.rstrip('\n') for s in filelist]    
+print(remove_newlines("python.txt"))  
