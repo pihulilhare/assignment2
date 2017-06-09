0b53c2f35cebf130a94ce59521c548b867a53c37
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-09 14:16:08 +0530

    Create assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,9 +1,9 @@
+import io
 file = open(“first.txt”,”w”) 
- 
 file.write(“Hello”) 
-file.write(“This is First  file”) 
-file.write(“Example of Python”) 
-file.write(“Learning Python is new Experience.”) 
+file.write(“This is First file”) 
+file.write(“Example of PythonProgram”) 
+file.write(“Learning Python new Experience.”) 
 file.seek(0,0)
 file.read()
 file.close() 
