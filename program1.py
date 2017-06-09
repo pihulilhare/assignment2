0ef8a56f16f5b4d52c3cd57dedf50410a341f703
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-09 14:51:21 +0530

    Create program1.py

--- program1.py
+++ program1.py
@@ -1,7 +1,13 @@
-
+#1. Write a Python program to read an entire text file and print its contents.
 file = open("python.txt", "w+")
 file.write( "This is First file.\Example of PythonProgram.\Learning Python new Experience.")
 file.seek(0,0)
 print file.read()
 print "Name of the file: ", file.name
 print "Closed or not : ", file.closed
+
+'''Output 
+[root@test assignment2]# python program1.py
+This is First file.\Example of PythonProgram.\Learning Python new Experience.
+Name of the file:  python.txt
+Closed or not :  False
