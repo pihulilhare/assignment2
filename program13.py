f72cd428d7f596670f7b77ef3c1aad1c032200ca
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-09 16:47:30 +0530

    Create program13.py

--- program13.py
+++ program13.py
@@ -1,5 +1,5 @@
 #Write a Python program to combine each line from first file with the corresponding line in second file. 
-with open('File2.txt') as fh2, open('File1.txt') as fh1:  
+with open('File2.txt') as f2, open('File1.txt') as f1:  
     for l1, l2 in zip(f2, f1):  
         # l1 from File2.txt, l2 from File1.txt
         print(l1+l2)  
