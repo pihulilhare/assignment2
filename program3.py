57b819a7714ecfb6c61deadf0c0606c16897714f
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-09 15:35:16 +0530

    Create program3.py

--- program3.py
+++ program3.py
@@ -1,1 +1,9 @@
-
+#3. Write a Python program to append text to a file and display the text. 
+def file_read(python):  
+        from itertools import islice  
+        with open(python, "w") as file:  
+                file.write("Python Exercises\n")  
+                file.write("Java Exercises")  
+        txt = open(python)  
+        print(txt.read())  
+file_read('python.txt')  
