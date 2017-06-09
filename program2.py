94b7819565e5e6e218f96b9909fa08c51869b136
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-09 15:16:16 +0530

    Create program2.py

--- program2.py
+++ program2.py
@@ -5,8 +5,8 @@ file.write( "This is First file.\nExample of PythonProgram.\nLearning Python new
 file.write( "This is Second file.\nExample of PythonProgram.\nLearning Python new Experience.")
 file.write( "This is Third file.\nExample of PythonProgram.\nLearning Python new Experience.")
 print file.read()
-print file.readline()
-#print file.readline(2)
+print file.readline():
+#print file.readline(2):
 #print file.readlines() 
 print "Name of the file: ", file.name
 print "Closed or not : ", file.closed
