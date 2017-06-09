0450de59f100b92a5d7fa9882258e7165e4fe71c
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-09 16:28:26 +0530

    Update program11.py

--- program11.py
+++ program11.py
@@ -1,7 +1,7 @@
 # Question 11 :Write a Python program to write a list to a file
 
 SkillSet = ['Andriod', 'Java', 'MongoDb', 'Python', 'Php', 'Ruby']   
- with open('File1.txt', "w") as file:   
+with open('File1.txt', "w") as file:   
   for file in SkillSet:   
    file.write("%s\n" % file)       
    content = open('File1.txt')   
