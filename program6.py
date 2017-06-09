d4ab63d754885cf586bd84bfa1492b315e05f957
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-09 16:00:25 +0530

    Create program6.py

--- program6.py
+++ program6.py
@@ -1,1 +1,6 @@
-
+#6. Write a Python program to read a file line by line store it into a variable. 
+def file_read(python):  
+        with open (python, "r") as file:  
+                data=file.readlines()  
+                print(data)  
+file_read('python.txt')  
