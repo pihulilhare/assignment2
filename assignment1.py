71a8e0c50c2466a9ce08a900716bb1fce4f5871e
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-09 14:36:55 +0530

    Delete assignment1.py

--- assignment1.py
+++
@@ -1,9 +0,0 @@
-import io
-file = open(“first.txt”,”w”) 
-file.write(“Hello”) 
-file.write(“This is First file”) 
-file.write(“Example of PythonProgram”) 
-file.write(“Learning Python new Experience.”) 
-file.seek(0,0)
-file.read()
-file.close() 
