b83c7f51db8f1a80f88276681b1da9d679933a97
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-09 16:38:48 +0530

    Create program12.py

--- program12.py
+++ program12.py
@@ -1,1 +1,8 @@
+#12  Write a Python program to copy the contents of a file to another file
 
+from shutil import copyfile
+
+import os
+
+os.system("touch File2.txt ")
+copyfile('File1.txt', 'File2.txt')
