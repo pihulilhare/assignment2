ad3513102fb7cba3e2023bd27231f6e9e210f326
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-09 16:45:43 +0530

    Create program12.py

--- program12.py
+++ program12.py
@@ -1,3 +1,4 @@
+
 #12  Write a Python program to copy the contents of a file to another file
 
 from shutil import copyfile
@@ -6,3 +7,20 @@ import os
 
 os.system("touch File2.txt ")
 copyfile('File1.txt', 'File2.txt')
+
+''' Output
+[root@test assignment2]# python program12.py
+[root@test assignment2]# cat File1.txt
+Andriod
+Java
+MongoDb
+Python
+Php
+Ruby
+[root@test assignment2]# cat File2.txt
+Andriod
+Java
+MongoDb
+Python
+Php
+Ruby'''
