4c854ac9c989bea1880ca7329c5cb03f50dc67e8
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-09 17:43:23 +0530

    Create program15.py

--- program15.py
+++ program15.py
@@ -6,3 +6,7 @@ print "Name of the file: ", f.name
 # Close opend file
 print "Closed or not : ", f.closed
 
+''' Output
+[root@test assignment2]# python program15.py
+Name of the file:  File3
+Closed or not :  False'''
