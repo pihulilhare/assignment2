7a10ce83e7c91e0ce6db6cd83080704d67650b63
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-09 17:32:23 +0530

    Create program14.py

--- program14.py
+++ program14.py
@@ -1,1 +1,12 @@
+# 14 : Write a Python program to read a random line from a file
+import random
+l = open("File3.txt").read().splitlines()
+print random.choice(l)
+
+
+'''
+output:
+[root@demo FileHandlingAssignment]# python program14.py
+apple
+'''
 
