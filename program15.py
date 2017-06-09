d19ab4180c6b87dc8363cf21e526450143897783
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-09 17:40:39 +0530

    Create program15.py

--- program15.py
+++ program15.py
@@ -1,6 +1,7 @@
+#15. Write a Python program to assess if a file is closed or not. 
 #Open a file
 f = open("File3", "wb")
 print "Name of the file: ", f.name
 
 # Close opend file
-f.close()
+print (f.close())
