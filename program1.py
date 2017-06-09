6900082560209be25e98efcd012fa3d17e2be549
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-09 14:47:44 +0530

    Create program1.py

--- program1.py
+++ program1.py
@@ -1,5 +1,7 @@
+
 file = open("python.txt", "w+")
 file.write( "This is First file.\Example of PythonProgram.\Learning Python new Experience.")
+file.seek(0,0)
 print file.read()
 print "Name of the file: ", file.name
 print "Closed or not : ", file.closed
