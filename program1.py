06a80c7a82399f916d686720ab35b2600a9ee201
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-09 14:43:01 +0530

    Create program1.py

--- program1.py
+++ program1.py
@@ -1,4 +1,4 @@
-file = open("program.txt", "w+")
+file = open("python.txt", "w+")
 file.write( "This is First file.\Example of PythonProgram.\Learning Python new Experience.")
 print file.read()
 print "Name of the file: ", file.name
