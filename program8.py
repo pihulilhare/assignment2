b49e35b4a5126e24b651d25211d17874114d8ee0
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-10 15:33:00 +0530

    Create program8.py

--- program8.py
+++ program8.py
@@ -1,11 +1,11 @@
 # 8. Write a python program to find the longest words. 
 
-def longest_word(python):  
-    with open(python, 'r') as infile:  
+def longest_word(File1):  
+    with open(File1, 'r') as infile:  
               words = infile.read().split()  
     maxlen = len(max(words, key=len))  
     return [word for word in words if len(word) == maxlen]  
   
-print(longest_word('python.txt')) 
+print(longest_word('File1.txt')) 
 
 ''' Output '''
