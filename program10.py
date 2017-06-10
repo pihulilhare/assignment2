d00d92d38ce3b6237eda1f51db79ccce0b76a5ed
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-10 15:42:35 +0530

    Create program10.py

--- program10.py
+++ program10.py
@@ -1,9 +1,21 @@
 #10. Write a Python program to get the file size of a plain file. 
-def file_read(python):  
+'''def file_read(python):  
         txt = open(python)  
         print(txt.read())  
-file_read('python.txt')  
+file_read('python.txt')  '''
+
+def file_size(python):  
+        import os  
+        statinfo = os.stat(python)  
+        return statinfo.st_size  
+  
+print("File size in bytes of a plain file: ",file_size("python.txt"))  
+
 '''output
 [root@test assignment2]# python program10.py
 Python Exercises
-Java Exercises'''
+Java Exercises
+Cloud Practice
+Basic Learning
+Andriod Training
+Framework Training'''
