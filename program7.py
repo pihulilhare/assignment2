e820270dc43a3b67c06685cd50351691ac0863a6
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-10 15:26:33 +0530

    Create program7.py

--- program7.py
+++ program7.py
@@ -7,3 +7,6 @@ def file_read(python):
                         content_array.append(line)  
                 print(content_array)  
 file_read('python.txt')  
+
+'''Output [root@test assignment2]# python program7.py
+['Python Exercises\n', 'Java Exercises\n', 'Cloud Practice\n', 'Basic Learning\n', 'Andriod Training\n', 'Framework Training\n']'''
