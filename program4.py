21bcd612c22b831c250cb969bdcc17cbd1a10a5f
pihulilhare <priyanka.lilhare@gmail.com>
2017-06-10 15:13:08 +0530

    Create program4.py

--- program4.py
+++ program4.py
@@ -18,3 +18,22 @@ def readFile(File4,lines):
                                         break  
   
 readFile('File4.txt',3)  
+
+'''Output
+
+"Failure is a part of life. From time to time you will fail at something. That does not mean that you should give up and never be happy." Clement Getate, Your Cross to Happiness 
+"Your life is not a thing, it's an experience; the fun comes from designing and enjoying the experience." Bill Burnett; Dave Evans, Designing Your Life: How to Build a Well-Lived, Joyful Life 
+"Sometimes, due to unforeseen tough times, we get discouraged and don't achieve our goals. As hard as it may seem, don't procrastinate in taking renewed action to reach your goals." Byron Pulsifer 
+"Everyone wishes for the better things of life, such as money, a good position, fame, and recognition; but most people never go far beyond the 'wishing' stage." Andrew Carnegie
+"Life is for the living, and there's a large difference between living and merely existing. When you can seize your potential and truly feel as if you are doing your best in this life, you are living." Patrick King, Limitless: Destroy Your Fears, Escape Your Comfort Zone, and Conquer Any Goal - Create The Life You Want 
+
+
+[root@test assignment2]# python program4.py
+"Sometimes, due to unforeseen tough times, we get discouraged and don't achieve our goals. As hard as it may seem
+, don't procrastinate in taking renewed action to reach your goals." Byron Pulsifer 
+"Everyone wishes for the better things of life, such as money, a good position, fame, and recognition; but most p
+eople never go far beyond the 'wishing' stage." Andrew Carnegie
+"Life is for the living, and there's a large difference between living and merely existing. When you can seize yo
+ur potential and truly feel as if you are doing your best in this life, you are living." Patrick King, Limitless:
+ Destroy Your Fears, Escape Your Comfort Zone, and Conquer Any Goal - Create The Life You Want '''
+
